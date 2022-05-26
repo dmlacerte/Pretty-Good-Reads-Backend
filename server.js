2af7279862b850bf1d617465e004625cd465ac29
dmlacerte <dmlacerte@gmail.com>
2022-05-26 12:21:53 -0400

    Initial User Authentication

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 require('dotenv').config()
 
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
+//user authentication
+const { OAuth2Client } = require('google-auth-library')
+const client = new OAuth2Client(process.env.CLIENT_ID)
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(cors())
 
 const bookControllers = require('./controllers/bookControllers')
 const ratingControllers = require('./controllers/ratingControllers')
@@ -18,6 +23,25 @@ app.use('/node_modules', express.static(__dirname + '/node_modules'))
 
 app.get('/', (req, res) => {
     res.send('<h1>we in</h1>')
+})
+
+//user authentication
+app.post('/api/v1/auth/google', async (req, res) => {
+    const { token }  = req.body
+    console.log(req.body)
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: process.env.CLIENT_ID
+    });
+    const { name, email, picture } = ticket.getPayload();    
+    // const user = await db.user.upsert({ 
+    //     where: { email: email },
+    //     update: { name, picture },
+    //     create: { name, email, picture }
+    // })
+    console.log(JSON.stringify(ticket.getPayload()));
+    res.status(201)
+    // res.json({ "name" : "test"})
 })
 
 const port = process.env.PORT || 4000
