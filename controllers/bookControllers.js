445529758ff6c7a7cc4e5876b4b44039f8980ae4
pzeise <phillip.zeise@gmail.com>
2022-05-27 22:20:02 -0400

    got search working

--- controllers/bookControllers.js
+++ controllers/bookControllers.js
@@ -5,15 +5,6 @@ const axios = require('axios')
 const Book = require('../models/book-model')
 const User = require('../models/user-model')
 
-router.get('/:id', (req, res) => {
-    Book.findById(req.params.id)
-    .then(book => {
-        //if (book) send it back
-        //else search API
-        res.json(book)
-    })
-    .catch(console.error)
-})
 
 //GET books associated with an individual user account
 router.get("/user/:userId", async (req, res) => {
@@ -28,6 +19,32 @@ router.get("/user/:userId", async (req, res) => {
         reading,
         finished
     });
+})
+
+router.get('/search/:type/:term/:index', async (req, res) => {
+    const searchType = req.params.type
+    let search = ''
+    if (searchType === 'title') {
+        search = `https://www.googleapis.com/books/v1/volumes?q=intitle:${req.params.term}&startIndex=${req.params.index}&printType=books&key=${process.env.API_KEY}`
+    } else if (searchType === 'author'){
+        search = `https://www.googleapis.com/books/v1/volumes?q=inauthor:${req.params.term}&startIndex=${req.params.index}&printType=books&key=${process.env.API_KEY}`
+    } else {
+        search = `https://www.googleapis.com/books/v1/volumes?q=subject:${req.params.term}&startIndex=${req.params.index}&printType=books&key=${process.env.API_KEY}`
+    }
+    
+    axios.get(search)
+    .then(element => res.json(element.data))
+    .catch(console.error)
+})
+
+router.get('/:id', (req, res) => {
+    Book.findById(req.params.id)
+    .then(book => {
+        //if (book) send it back
+        //else search API
+        res.json(book)
+    })
+    .catch(console.error)
 })
 
 router.post('/post', (req, res) => {
