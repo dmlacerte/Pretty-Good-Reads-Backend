66c47d1b7badde88c09b90fca7f70f50ab8050c3
dmlacerte <dmlacerte@gmail.com>
2022-05-28 21:12:26 -0400

    Front end cookie handling

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ require('dotenv').config()
 
 const express = require('express')
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+    origin : 'http://localhost:3000',
+    credentials: true
+}))
+app.use(cookieParser())
 
 const bookControllers = require('./controllers/bookControllers')
 const ratingControllers = require('./controllers/ratingControllers')
@@ -24,6 +29,6 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 4000
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log(`Pretty Good Reads Back End is running on port ${port}`)
 })
